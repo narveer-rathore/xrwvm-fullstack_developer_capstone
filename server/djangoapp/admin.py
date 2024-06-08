2422d28bc5c103f0f6755431dfa10e7d0f4f70e1
Narveer Rathore <rathore.narveer1994@gmail.com>
2024-06-08 13:20:45 -0400

    adding deployment.yaml

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -5,11 +5,3 @@ from .models import CarMake, CarModel
 # Register your models here.
 admin.site.register(CarMake)
 admin.site.register(CarModel)
-
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
